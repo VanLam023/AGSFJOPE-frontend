25e9e7ffe710cefcb8be7deb4f404b7cc5d4599a
SpaceCattStalin <spacestalincyka@gmail.com>
2026-03-15 20:23:03 +0700

    Add environment variable reading.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default ({ mode }) => {
+  // Đỏ nhưng mà xài được, để tải biến môi trường
+  const loadedEnv = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    define: {
+      // Biến môi trường cho url backend
+      __BASE_URL__: JSON.stringify(loadedEnv.VITE_API_BASE_URL),
+    },
+    plugins: [react()],
+  });
+};
+
+// export default defineConfig({
+//   define: {
+//   },
+//   plugins: [react()],
+// });
